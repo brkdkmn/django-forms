d3333aee8387144f21801bf846d397919b06d543
burak <bydkmnn@gmail.com>
2021-10-15 16:42:35 +0300

    .add

---
+++ student/views.py
@@ -0,0 +1,40 @@
+from student.models import Student
+from .forms import StudentForm
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+
+def index(request):
+    return render(request, 'student/index.html')
+
+
+def student_page(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Student added successfully")
+            return redirect('student')
+        else :
+            messages.error(request, "Validation failed")
+            
+    context = {
+        'form': form
+    }
+    return render(request, 'student/student.html', context)
+
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES)
+#         if form .is_valid():
+#             form.save()
+#             messages.success(request, "Student added successfully")
+#             return redirect('student') #posttan çıkarıp tekrar gete soktuk.
+#         else :
+#             messages.error(request, "Validation failed")
+#     context = {
+#         'form': form
+#     }
+#     return render(request,'student/student.html', context)
